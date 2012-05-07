a464d648e6da97c7c25bee7fcd35b56f3b730f8c
Grambo <grambo@grambo.me.uk>
2012-05-07 20:26:05 +0100

    Refactored out generic prompt method in setup.py

--- setup.py
+++ setup.py
@@ -8,53 +8,77 @@ import urllib
 import zipfile
 import re
 
-def LinkFile( orig, target, prompt=True, force=False, mkdirs=True ):
-    actualTarget = os.path.expanduser( target )
-    shouldBackup = False
-    if os.path.exists( actualTarget ):
-        if os.path.islink( actualTarget ):
-            if( os.path.samefile( actualTarget, orig ) ):
-            	print "%s is already symlinked in place." % orig
-            	return
-        if prompt:
-            print "%s already exists" % target
-            while True:
-                print "Action? r(eplace), s(kip), q(uit), d(iff)"
-                action = raw_input()
-                if action in [ 'r', 'replace' ]:
-                    break
-                elif action in [ 's', 'skip' ]:
-                    return
-                elif action in [ 'q', 'quit' ]:
-                    quit()
-                elif action in [ 'd', 'diff' ]:
-                    origFullPath = os.path.abspath( orig )
-                    subprocess.call(
-                            [ 'diff', '-u', actualTarget, origFullPath ],
-                            stdout = sys.stdout,
-                            stderr = sys.stderr
-                            )
-        elif not force:
-            return
-        os.rename( actualTarget, actualTarget + '.orig' )
-    parentDir = os.path.dirname( actualTarget ) 
+def Prompt( prompt, options ):
+    '''
+    Prompts the user for input
+    @param: prompt    Initial text for the prompt
+    @param: options   A dict mapping options to functions
+    @return:    Returns the same as the selected actionFunc
+    '''
+    entryToActionMap = []
+    optionsList = []
+    usedPrefixes = []
+    for (text,func) in options.iteritems():
+        entries = [ text ]
+        if text[0] not in usedPrefixes:
+            text = text[0] + '(%s)' % text[1:]
+            entries.append( text[0] )
+            usedPrefixes.append( text[0] )
+        entryToActionMap.append( (entries,func) )
+        optionsList.append( text )
+    while True:
+        action = raw_input( prompt + ', '.join( optionsList ) )
+        for (entries,actionFunc) in entryToActionMap:
+            if action in entries:
+                return actionFunc()
+
+def DoLink( orig, target ):
+    '''
+    Links a file in place, making a backup first
+    @param: orig    The file to link from
+    @param: target  The target path
+    '''
+    if os.path.exists( target ):
+        os.rename( target, target + '.orig' )
+    parentDir = os.path.dirname( target ) 
     if not os.path.exists( parentDir ):
-        if mkdirs:
-            print "Making path %s" % parentDir
-            os.makedirs( parentDir )
-        else:
-            print "Error: %s does not exist" % parentDir
-            return
-    print "Linking %s to %s" % ( orig, actualTarget )
+        print "Making path %s" % parentDir
+        os.makedirs( parentDir )
+    print "Linking %s to %s" % ( orig, target )
     if 'symlink' in dir( os ):
-        os.symlink( os.path.abspath( orig ), actualTarget )
+        os.symlink( os.path.abspath( orig ), target )
     else:
         # Probably just python < 3.2 on windows.
         # Fall back on mklink
         subprocess.check_call( [ 
-            'mklink', actualTarget, os.path.abspath( orig )
+            'mklink', target, os.path.abspath( orig )
             ], shell=True )
 
+def LinkFile( orig, target ):
+    actualTarget = os.path.expanduser( target )
+    shouldBackup = False
+    if not os.path.exists( actualTarget ):
+        DoLink( orig, actualTarget )
+    else:
+        if os.path.islink( actualTarget ):
+            if( os.path.samefile( actualTarget, orig ) ):
+            	print "%s is already symlinked in place." % orig
+            	return
+        print "%s already exists" % target
+        def DoDiff():
+            origFullPath = os.path.abspath( orig )
+            subprocess.call(
+                    [ 'diff', '-u', actualTarget, origFullPath ],
+                    stdout = sys.stdout,
+                    stderr = sys.stderr
+                    )
+        Prompt( 'Action? ', {
+            'replace' : lambda: DoLink( orig, actualTarget ),
+            'skip' : lambda: None,
+            'quit' : quit,
+            'diff' : DoDiff
+            } )
+    
 def Unzip( filename, destPath ):
     if not os.path.exists( filename ):
         print "Unzip Error: File %s does not exist" % filename
@@ -84,6 +108,8 @@ class Common(object):
     dotfiles = {}
     vimDir = os.path.expanduser( os.path.join( '~', '.vim' ) )
     vimTmp = os.path.join( vimDir, 'tmp' )
+    configFile = 'dotfiles.conf'
+    configExampleFile = 'dotfiles.conf'
 
     # Vim Plugins from vim.org
     vimOrgPlugins = {
@@ -108,6 +134,10 @@ class Common(object):
     def __init__( self, options ):
         self.bundlePath = os.path.join( self.vimDir, 'bundle', '' )
         self.clean = options.clean
+        self.LoadConfig()
+
+    def LoadConfig( self ):
+        pass
 
     def Install(self):
         """ Installs everything """
