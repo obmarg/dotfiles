51c93aa5296de989ace17f5836933c8e7185694f
Graeme Coupar <grambo@grambo.me.uk>
2015-05-16 16:28:53 +0100

    Added rust spacemacs layer.
    Copied from
    https://github.com/sgnr/dotfiles/tree/master/spacemacs-layers/rust

---
+++ spacemacs-layers/rust/packages.el
@@ -0,0 +1,17 @@
+;;; packages.el --- rust Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar rust-packages '(rust-mode))
+
+(defvar rust-excluded-packages '())
+
+(defun rust/init-rust-mode ())
