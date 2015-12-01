5b35e0ce998bbeaabc15473b8caaff6d82736a3a
Graeme Coupar <grambo@grambo.me.uk>
2015-12-01 00:26:22 +0000

    Add elixir-dogma spacemacs layer.

---
+++ spacemacs.d/elixir-dogma/packages.el
@@ -0,0 +1,14 @@
+(setq elixir-dogma-packages
+      '(flycheck
+        (flycheck-elixir-dogma
+         :fetcher github
+         :repo "obmarg/flycheck-elixir-dogma"
+         )))
+
+(defun elixir-dogma/init-flycheck-elixir-dogma ()
+  (use-package flycheck-elixir-dogma
+               :defer t
+               :init (add-hook 'elixir-mode-hook 'flycheck-elixir-dogma-setup)))
+
+(defun elixir-dogma/post-init-flycheck ()
+  (add-hook 'elixir-mode-hook 'flycheck-mode))
