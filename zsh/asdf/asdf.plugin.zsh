97c75fd4a61bd1e508fb90e70815389ff49eadaf
Graeme Coupar <grambo@grambo.me.uk>
2016-01-04 12:31:14 +0000

    zsh prompts to install asdf if not installed.

---
+++ zsh/asdf/asdf.plugin.zsh
@@ -0,0 +1,45 @@
+if [ ! -d ~/.asdf ] ; then
+    echo "ASDF is not installed"
+    read -q "REPLY?Do you wish to install? [y/n] "
+    echo
+    if [[ $REPLY == "y" ]] ; then
+        git clone https://github.com/HashNuke/asdf.git ~/.asdf
+    fi
+    echo
+fi
+
+if [ -s ~/.asdf/asdf.sh ] ; then
+    . ~/.asdf/asdf.sh
+
+    ASDF_PLUGINS=`asdf plugin-list`
+
+    if [[ ! $ASDF_PLUGINS == *"elixir"* ]] ; then
+        echo "ASDF elixir plugin is not installed"
+        read -q "REPLY?Do you wish to install? [y/n] "
+        echo
+        if [[ $REPLY == "y" ]] ; then
+            asdf plugin-add elixir https://github.com/HashNuke/asdf-elixir.git
+        fi
+        echo
+    fi
+
+    if [[ ! $ASDF_PLUGINS == *"erlang"* ]] ; then
+        echo "ASDF erlang plugin is not installed"
+        read -q "REPLY?Do you wish to install? [y/n] "
+        echo
+        if [[ $REPLY == "y" ]] ; then
+            asdf plugin-add erlang https://github.com/HashNuke/asdf-erlang.git
+        fi
+        echo
+    fi
+
+    if [[ ! $ASDF_PLUGINS == *"node"* ]] ; then
+        echo "ASDF node plugin is not installed"
+        read -q "REPLY?Do you wish to install? [y/n] "
+        echo
+        if [[ $REPLY == "y" ]] ; then
+            asdf plugin-add node https://github.com/HashNuke/asdf-nodejs.git
+        fi
+        echo
+    fi
+fi
