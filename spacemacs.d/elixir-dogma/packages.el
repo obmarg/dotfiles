2d2e151006c8e8a0c26ee70281965afac9226e94
Graeme Coupar <grambo@grambo.me.uk>
2016-02-29 13:39:38 +0000

    Fix elixir-dogma spacemacs layer.
    The syntax for defining where packages come from seems to have changed,
    and I'm also explicitly adding a `(use-package flycheck)` to ensure that
    flycheck is loaded before the plugin.

--- spacemacs.d/elixir-dogma/packages.el
+++ spacemacs.d/elixir-dogma/packages.el
@@ -1,11 +1,12 @@
 (setq elixir-dogma-packages
       '(flycheck
         (flycheck-elixir-dogma
-         :fetcher github
-         :repo "obmarg/flycheck-elixir-dogma"
-         )))
+         :location (recipe
+                    :fetcher github
+                    :repo "obmarg/flycheck-elixir-dogma"))))
 
 (defun elixir-dogma/init-flycheck-elixir-dogma ()
+  (use-package flycheck)
   (use-package flycheck-elixir-dogma
                :defer t
                :init (add-hook 'elixir-mode-hook 'flycheck-elixir-dogma-setup)))
