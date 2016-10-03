ab3c1edcf26b66930ad510f63e00375e83e5bcf3
Graeme Coupar <grambo@grambo.me.uk>
2016-10-03 14:47:44 +0100

    Update hjson extension for spacemacs v0.200

--- spacemacs.d/hjson/packages.el
+++ spacemacs.d/hjson/packages.el
@@ -1,3 +1,12 @@
 (setq hjson-packages
       '(company
-        flycheck))
+        flycheck
+        (hjson-mode :location local)
+        (flycheck-hjson :location local)))
+
+(defun hjson/init-hjson-mode ()
+  (use-package hjson-mode))
+
+(defun hjson/init-flycheck-hjson ()
+  (use-package flycheck-hjson
+    :init (spacemacs/add-flycheck-hook 'hjson-mode-hook)))
