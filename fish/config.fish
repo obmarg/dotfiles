e91a78c0e6a664d79869aee8df2663d7d0126066
Graeme Coupar <grambo@grambo.me.uk>
2020-07-06 13:09:12 +0100

    Fix cdr alias for fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ abbr --erase gst
 abbr -a gst git st
 
 alias gpub='git push -u origin (git rev-parse --abbrev-ref HEAD)'
-alias cdr='cd (git rev-parse --show-toplevel 2> /dev/null)
+alias cdr='cd (git rev-parse --show-toplevel 2> /dev/null)'
 
 if type -q sk
     skim_key_bindings
