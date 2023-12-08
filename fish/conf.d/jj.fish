bf8b42fe2d938797ce2952a99b47a8f5160ebc21
Graeme Coupar <grambo@grambo.me.uk>
2023-12-08 13:25:57 +0000

    add some jj aliases to fish config

---
+++ fish/conf.d/jj.fish
@@ -0,0 +1,9 @@
+alias jjl='jj l'
+alias jjs='jj s'
+
+alias jjf='jj git fetch'
+alias jjp='jj git push'
+alias jjpc='jj git push -c @'
+alias jjd='jj describe'
+alias jjn='jj new'
+alias jjr='jj rebase'
