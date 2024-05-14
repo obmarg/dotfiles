37137e91b6f52a44519ca2df01ed377141233709
Graeme Coupar <graeme@bop.lan>
2024-05-14 17:17:10 +0100

    extract darwin config a bit

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -7,67 +7,13 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.curl
-          pkgs.difftastic
-          pkgs.direnv
-          pkgs.dust
-          pkgs.gh
-          pkgs.git
-          pkgs.jujutsu
-          pkgs.ripgrep
-          pkgs.wget
-          pkgs.atuin
-          pkgs.neovim
-          pkgs.starship
-          pkgs.tokei
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      nix.settings.substituters = [
-	"https://cache.lix.systems"
-      ];
-
-      nix.settings.trusted-public-keys = [
-        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      ];
-
-      nix.extraOptions = ''
-        extra-nix-path = nixpkgs=flake:nixpkgs
-      '';
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#bop
     darwinConfigurations."bop" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        ./darwin.nix
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
