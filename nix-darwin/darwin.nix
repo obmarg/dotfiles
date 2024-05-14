721273581d9ede474ea412161fb16e78b2fd208b
Graeme Coupar <graeme@bop.lan>
2024-05-14 17:26:01 +0100

    remove broken line

--- nix-darwin/darwin.nix
+++ nix-darwin/darwin.nix
@@ -42,7 +42,7 @@
       programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+      # system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
