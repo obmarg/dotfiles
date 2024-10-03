588883f08469ab26d4ae61d3134bebd64cc7f789
Graeme Coupar <grambo@grambo.me.uk>
2024-10-03 10:40:11 -0400

    update wezterm a bit

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,10 @@
+
+local wezterm = require 'wezterm'
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+
+local act = wezterm.action
 
 -- This is where you actually apply your config choices
 
@@ -15,6 +20,11 @@ config.font_size = 16
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 config.enable_scroll_bar = true
+
+config.keys = {
+  { key = 'LeftArrow', mods = 'SUPER', action = act.ActivateTabRelative(-1) },
+  { key = 'RightArrow', mods = 'SUPER', action = act.ActivateTabRelative(1) } 
+}
 
 config.send_composed_key_when_left_alt_is_pressed = true
 
