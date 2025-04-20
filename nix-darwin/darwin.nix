353556608bf78ca8d550abeab04237e192a8657e
Graeme Coupar <grambo@grambo.me.uk>
2025-04-20 20:38:13 +0100

    remove this, not sure why its not working anymore

--- nix-darwin/darwin.nix
+++ nix-darwin/darwin.nix
@@ -19,10 +19,6 @@
           pkgs.nushell
         ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
